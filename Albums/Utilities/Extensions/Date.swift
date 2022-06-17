6ef6a3c1fef772f40c6b57a6d18d425347f429e4
KAKPONOTAM <sabitovdanill@icloud.com>
2022-06-17 10:58:21 +0300

    changed_dateFormat

--- Albums/Utilities/Extensions/Date.swift
+++ Albums/Utilities/Extensions/Date.swift
@@ -3,9 +3,11 @@ import Foundation
 extension Date {
     static func changeStringDateFormat(releaseDate: String) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "YYYY-MM-dd"
+        let newFormatter = ISO8601DateFormatter()
+        guard let date = newFormatter.date(from: releaseDate) else { return "danil ti loh" }
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         dateFormatter.dateStyle = .medium
-        guard let date = dateFormatter.date(from: releaseDate) else { return "idk what is this date" }
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
         dateFormatter.dateFormat = "d MMMM, yyyy"
         return dateFormatter.string(from: date)
     }
